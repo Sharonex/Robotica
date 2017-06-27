b5b6b97fc18cde96cf47b27bdb8b13030afd8e5e
sharon <sharonavni7@gmail.com>
2017-06-27 18:19:12 +0300

    Deletes all files from repo

--- ObstacleAvoid/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lHamsterAPICommon -lopencv_core -lopencv_highgui -lCogniAlgCPP -lHamsterAPIClientCPP
-
